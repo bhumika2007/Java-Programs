24e581f03738a67783d2140eccad76d73ed07282
Bhumika <160400051+bhumika2007@users.noreply.github.com>
2024-04-17 17:18:41 +0530

    Static or non static in java

--- Calcirunner.java
+++ Calcirunner.java
@@ -1,20 +1,17 @@
-class Calci 
-{
-    public static void sum(){
-        System.out.println("welcome to sum");
-    }
-      public void sub(){
-        System.out.println("welcome to sub");
-      }
-    }
- public class Calcirunner
- {
-    public static void main (String a[])
-    {
-        Calci.sum();
-        Calci aa=new Calci();
-        aa.sub();
-    }
- } 
-    
-    
+class Calci {
+  public static void sum() {
+    System.out.println("welcome to sum");
+  }
+
+  public void sub() {
+    System.out.println("welcome to sub");
+  }
+}
+
+public class Calcirunner {
+  public static void main(String a[]) {
+    Calci.sum();
+    Calci aa = new Calci();
+    aa.sub();
+  }
+}
