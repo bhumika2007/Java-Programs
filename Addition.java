6c2901ad81959b4e15bd07092b645cd8a8f90792
Bhumika <160400051+bhumika2007@users.noreply.github.com>
2024-04-17 17:15:59 +0530

    Initial commit

--- Addition.java
+++ Addition.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
-class Addition 
-{
-    public static void main(String args[])
-    {
-        int a,b,c;
+
+class Addition {
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        int a, b, c;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number:");
-        a=sc.nextInt();
+        a = sc.nextInt();
         System.out.println("Enter Second Number:");
-        b = aa.nextInt();
+        b = sc.nextInt();
         c = a + b;
         System.out.println("Addition=" + c);
     }
