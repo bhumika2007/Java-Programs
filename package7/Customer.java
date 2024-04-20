4876fac75fc30de92c8c2a21fb27ab68ad8d4cba
Bhumika <160400051+bhumika2007@users.noreply.github.com>
2024-04-20 17:36:11 +0530

    Added programs

---
+++ package7/Customer.java
@@ -0,0 +1,20 @@
+package package7;
+
+public class Customer {
+    private String name;
+    private String phoneNumber;
+
+    public Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
---
+++ package7/Pricing.java
@@ -0,0 +1,12 @@
+package package7;
+
+public class Pricing {
+    private static final double BASE_PRICE = 100.00;
+
+    public static double calculatePrice(int seatNumber) {
+        if (seatNumber % 3 == 0) {
+            return BASE_PRICE * 1.30;
+        }
+        return BASE_PRICE;
+    }
+}
